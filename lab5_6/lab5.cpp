7360a4ff7c73ae551e12f413bde08d257fcf4eed
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-20 12:02:16 -0500

    Create lab5.cpp

---
+++ lab5_6/lab5.cpp
@@ -0,0 +1,20 @@
+// Driver File for Fruit.h
+// Main Driver for Fruit.h in Lab 5
+
+#include "Fruit.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    std::cout << "Driver for Fruit.h files in lab5." << std::endl;
+    Fruit apple = Fruit("Apple, red delicious, with skin, raw", 100, 62, 14.8, .21, .21);
+    Fruit banana = Fruit("Banana, overripe, raw", 100, 85, 20.1, .22, .73);
+    Fruit kiwi = Fruit("Kiwi", 100, 58, .14, .44, 1.06);
+    apple.to_string();
+    banana.to_string();
+    kiwi.to_string();
+
+    return 0;
+}
