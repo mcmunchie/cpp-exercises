9b85cb0fa93bb667034f6c08c88d0a4eb76e860f
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-21 17:11:07 -0500

    Create Person.h

---
+++ lab9/Person.h
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#ifndef PERSON_PERSON_H
+#define PERSON_PERSON_H
+
+class Person{
+public:
+  Person();
+  Person(int);
+  Person(int, std::string);
+  Person(int, std::string, std::string);
+
+  // print out the Person's data
+  void print_person_data();
+
+private:
+  int birth_year = 0;
+  std::string hometown;
+  std::string name;
+
+protected:
+  void set_birth_year(int);
+  void set_hometown(std::string);
+  void set_name(std::string);
+
+  int get_birth_year() const;
+  std::string get_hometown();
+  std::string get_name();
+};
+
+#endif // PERSON_PERSON_H
